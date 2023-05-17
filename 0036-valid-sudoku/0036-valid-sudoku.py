a33b0734a74b9f040b209776685de4fd8e9b2217
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-05-18 04:36:24 +0530

    Time: 126 ms (9.75%), Space: 16.3 MB (14.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,60 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        global temp
-        temp = [0]*9
-        def validRows(row):
-            global temp
-            for i in range(9):
-                if board[row][i].isnumeric():
-                    if temp[int(board[row][i]) - 1] > 0:
-                        return False
-                    temp[int(board[row][i]) - 1] += 1
-
-            temp = [0] * 9
-            return True
-
-        def validCols(cols):
-            global temp
-            for i in range(9):
-                    if board[i][cols].isnumeric():
-                        if temp[int(board[i][cols]) - 1] > 0:
-                            return False
-                        temp[int(board[i][cols]) - 1] += 1
-            temp = [0] * 9
-            return True
-
-
-        def validBox(row,col):
-            global temp
-            for i in range(3):
-                if board[row][col + i].isnumeric():
-                    if temp[int(board[row][col + i]) - 1] > 0:
-                        return False
-                    temp[int(board[row][col + i]) -1] += 1
-                if board[row + 1][col + i].isnumeric():
-                    if temp[int(board[row + 1][col + i]) - 1] > 0:
-                        return False
-                    temp[int(board[row + 1][col + i]) -1] += 1
-                if board[row + 2][col + i].isnumeric():
-                    if temp[int(board[row + 2][col + i]) - 1] > 0:
-                        return False
-                    temp[int(board[row + 2][col + i]) -1] += 1
-
-            temp = [0] * 9
-            return True
-
-        for i in range(9):
-            if not validRows(i):
-                return False
-            if not validCols(i):
-                return False
-        for i in [0,3,6]:
-            if not validBox(i,3):
-                return False
-            if not validBox(i,0):
-                return False
-            if not validBox(i,6):
-                return False
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+        
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or 
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+        
         return True
             
             
