e6d865fa07fcfcdcfafa3439ae1208fba7b2d9a8
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-05-21 02:29:47 +0530

    Time: 29 ms (89.20%), Space: 16.4 MB (25.09%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,6 +1,7 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
-            res = []
+
+        """ res = []
             curr_line = []
             width = 0
             i = 0
@@ -31,6 +32,17 @@ class Solution:
             curr_line[-1] += " " * spaces
 
             res.append("".join(curr_line))
-            return res
+            return res"""
+
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
 
         
