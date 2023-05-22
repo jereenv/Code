4c43f553c232dcf5b8a7c50759e23a0ff89a804c
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-05-23 00:17:37 +0530

    Time: 88 ms (27.60%), Space: 16.6 MB (6.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,15 +5,16 @@ class Solution:
         maxC = len(image[0])
         srC = image[sr][sc]
 
-        def dfs(r, c):
-            if r < 0 or r >= maxR or c < 0 or c >= maxC or image[r][c] != srC or image[r][c] == color:
+        def dfs(r,c):
+            if srC == color:
                 return
-
-            image[r][c] = color
-            dfs(r+1, c)
-            dfs(r-1, c)
-            dfs(r, c-1)
-            dfs(r, c+1)
+            
+            if r >= 0 and r < maxR and c >= 0 and c < maxC and image[r][c] == srC :
+                image[r][c] = color
+                dfs(r+1,c)
+                dfs(r-1,c)
+                dfs(r,c-1)
+                dfs(r,c+1)
         dfs(sr,sc)
         return image
             
