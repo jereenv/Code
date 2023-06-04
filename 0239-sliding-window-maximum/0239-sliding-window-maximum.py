0cffe247f50c55feb07a1aa524abdcdd051eac99
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-06-04 21:26:26 +0530

    Time: 1706 ms (55.16%), Space: 31.9 MB (57.80%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -4,17 +4,17 @@ class Solution:
         l = 0
         r = 0
         
-        q = []
+        q = collections.deque()
         
         ans = []
         while r < len(nums):
             
             while q and nums[q[-1]] < nums[r]:
-                q.pop(-1)
+                q.pop()
             q.append(r)
             
             if l > q[0]:
-                q.pop(0)
+                q.popleft()
             
             if r + 1 >= k:
                 ans.append(nums[q[0]])
