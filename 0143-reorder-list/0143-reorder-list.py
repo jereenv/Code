0f0c68788566376edfc008a16323cae7d22195f2
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-06-05 04:48:22 +0530

    Time: 108 ms (27.26%), Space: 26.3 MB (21.91%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,31 +8,20 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        # Edge case - the list is guaranteed to be non-empty
         if not head.next:
             return head
         
-        # General case
         slow, fast = head, head
         while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         
         slow.next, slow = None, slow.next
-        # temp = slow.next
-        # slow.next = None
-        # slow = temp
         slow = self._reverseList(slow)
 
         while slow and head:
             head.next, head = slow, head.next
             slow.next, slow = head, slow.next
-            # temp = head.next
-            # head.next = slow
-            # head = temp
-            # temp = slow.next
-            # slow.next = head
-            # slow = temp
     
     def _reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         pre, cur, nex = None, head, head.next
