05e21e3b88f920bc2be1255683ff2aa2a489f5ae
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-06-07 01:32:47 +0530

    Time: 131 ms (69.95%), Space: 18.3 MB (64.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -8,17 +8,17 @@ class Solution:
         l_max = 0
         r_max = 0
         
-        # we can stop when l == r as r is max_r and hence it would just add 0 to result (line 24)
+        # we can stop when l == r as r is max_r and hence it would just add 0 to result (line 23)
         while l < r:
             
             if h[l] < h[r]:
-                if l_max <= h[l]:
+                if l_max < h[l]:
                     l_max = h[l]
                 else:
                     result += l_max - h[l]
                 l += 1
             else:
-                if r_max <= h[r]:
+                if r_max < h[r]:
                     r_max = h[r]
                 else:
                     result += r_max - h[r]
