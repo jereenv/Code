181da1514c415efed23d58dc219ff2ca9d051a99
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-08 12:31:12 -0700

    Time: 889 ms (79.33%), Space: 32.4 MB (72.14%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        
+        first_num = second_num = 2**31
+        for i in nums:
+            if i <= first_num:
+                first_num = i
+            elif i <= second_num:
+                second_num = i
+            else:
+                return True
+        return False
+                
+            
+            
+        
