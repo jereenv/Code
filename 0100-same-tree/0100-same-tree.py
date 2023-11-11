eccf9eafffe819f6dc7211c98a902d39d4fcf455
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-10 17:08:46 -0700

    Time: 43 ms (21.46%), Space: 16.3 MB (21.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -11,9 +11,7 @@ class Solution:
             
             if not node1 and not node2:
                 return True
-            if not node1 or not node2:
-                return False
-            if node1.val != node2.val:
+            if not node1 or not node2 or node1.val != node2.val:
                 return False
             
             return dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
