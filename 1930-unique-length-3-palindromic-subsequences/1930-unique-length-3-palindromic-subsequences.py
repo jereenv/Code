507a73a19e93b8a1759aba349e29497856bc9349
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-13 17:26:46 -0700

    Time: 312 ms (58.12%), Space: 17.9 MB (15.24%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,15 +1,24 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        letters = set(s)
+        
+        vis = set()
         ans = 0
         
-        for letter in letters:
-            i, j = s.index(letter), s.rindex(letter)
-            between = set()
+        n =  len(s) - 1
+        
+        for i in range(len(s)):
+            l, r = i, n
             
-            for k in range(i + 1, j):
-                between.add(s[k])
-            
-            ans += len(between)
+            if s[l] not in vis:
+                while l < r:
+                    if s[r] != s[l]:
+                        r -= 1
+                    else:
+                        s1 = set(list(s[l+1:r]))
+                        for letter in s1:
+                            ans += 1
+
+                        break
+                vis.add(s[l])
 
         return ans
