b1955804fae0e2001b726700338b4b2b478c8681
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-15 18:41:41 -0700

    Time: 37 ms (82.15%), Space: 16.3 MB (42.98%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -7,13 +7,11 @@ class Solution:
         #         if a[i] == b[i]:
         
         ans = nums[0]
-        nums = set(nums)
+        # nums = set(nums)
         
         for i in range(len(ans)):
             
             t = "1" if ans[i] == "0" else "0"
-            
-            
             temp = ans[:i] + t + ans[i+1:]
             if temp not in nums:
                 return temp
