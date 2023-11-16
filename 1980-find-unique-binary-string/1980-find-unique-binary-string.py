8266fc6960013cfbb8415383a45966da3bbe22ab
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-15 18:42:15 -0700

    Time: 36 ms (86.51%), Space: 16.4 MB (42.98%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,20 +1,14 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        # n = len(nums)
-        # def xor(a,b):
-        #     ans = 
-        #     for i in range(nums):
-        #         if a[i] == b[i]:
+        integers = set()
+        for num in nums:
+            integers.add(int(num, 2))
+
+        ans = int(nums[0], 2)
+        n = len(nums)
+
+        while ans in integers:
+            ans = random.randrange(0, 2 ** n)
         
-        ans = nums[0]
-        nums = set(nums)
-        
-        for i in range(len(ans)):
-            
-            t = "1" if ans[i] == "0" else "0"
-            temp = ans[:i] + t + ans[i+1:]
-            if temp not in nums:
-                return temp
-                    
-            
-        
+        s = bin(ans)[2:]
+        return "0" * (n - len(s)) + s
