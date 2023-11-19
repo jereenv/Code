430c4a4716cce88135d0802e21a32c7b2c134b36
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-18 19:54:36 -0700

    Time: 38 ms (61.78%), Space: 16.4 MB (12.67%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -1,33 +1,28 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def swapPairs(self, head: ListNode) -> ListNode:
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        # If the list has no node or has only one node left.
         if not head or not head.next:
             return head
-        
-        
-        
-        prev = ListNode(head)
-        newH = prev
-        curr = head
-        next = head.next
-        
-        while curr and curr.next:
-            
-            next = curr.next
-            
-            prev.next = next
-            curr.next = next.next
-            next.next = curr
-            
-            
-            prev = curr
-            curr = curr.next
 
-        
-        return newH.next
-            
+        # Nodes to be swapped
+        first_node = head
+        second_node = head.next
+
+        # Swapping
+        first_node.next  = self.swapPairs(second_node.next)
+        second_node.next = first_node
+
+        # Now the head is the second node
+        return second_node
