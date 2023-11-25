1a3861310294eebbef5c5767bc09d1d289661bd4
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-25 16:12:47 -0700

    Time: 701 ms (87.90%), Space: 31.7 MB (75.48%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.py
@@ -7,14 +7,8 @@ class Solution:
         lsum = 0
         
         for idx in range(n):
-            
             ans.append(rsum - (n - idx)*nums[idx] + idx*nums[idx] - lsum)
-            
             rsum -= nums[idx]
             lsum += nums[idx]
-            
-            
-
-        
         return ans
         
