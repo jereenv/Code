90e915eca95ffb6997442581504952baa5bbdc4e
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-25 16:17:20 -0700

    Time: 155 ms (91.16%), Space: 16.6 MB (64.75%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -1,24 +1,15 @@
 class Solution:
     def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
         def check(arr):
-            min_element = min(arr)
-            max_element = max(arr)
-
-            if (max_element - min_element) % (len(arr) - 1) != 0:
-                return False
-
-            diff = (max_element - min_element) / (len(arr) - 1)
+            arr.sort()
+            diff = arr[1] - arr[0]
             
-            arr_set = set(arr)
-            curr = min_element + diff
-            while curr < max_element:
-                if curr not in arr_set:
+            for i in range(2, len(arr)):
+                if arr[i] - arr[i - 1] != diff:
                     return False
                 
-                curr += diff
-            
             return True
-
+        
         ans = []
         for i in range(len(l)):
             arr = nums[l[i] : r[i] + 1]
