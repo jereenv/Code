6621f0673e716926b4bc53d9ebe32834cb256646
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-24 17:41:37 -0700

    Time: 85 ms (19.79%), Space: 17.2 MB (59.11%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.py
@@ -5,18 +5,6 @@ class Solution:
         for i in s:
             arr[ord(i) - ord('a')] += 1
         
-#         print(freq1)
-        
-#         freq = {}
-#         for i in s:
-#             if i not in freq:
-#                 freq[i] = 0
-#             freq[i] += 1
-
-#         arr = []
-        # for i in freq:
-        #     arr.append(freq[i])
-        
         ans = 0
         arr.sort(reverse = True)
         
