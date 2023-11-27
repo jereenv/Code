12260506e72862bdb634b50f8b610c7650d30c8c
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-11-27 02:47:40 -0700

    Time: 2182 ms (32.87%), Space: 86.8 MB (15.23%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,32 +1,94 @@
 class Solution:
+    
+    ans = 0
     def knightDialer(self, n: int) -> int:
-        @cache
-        def dp(remain, square):
-            if remain == 0:
+        
+        paths = {
+            0: [4,6],
+            1: [6, 8],
+            2: [7, 9],
+            3: [4, 8],
+            4: [0, 3, 9],
+            5: [],
+            6: [1, 7, 0],
+            7: [2, 6],
+            8: [1, 3],
+            9: [4, 2]
+        }
+        
+        memo = {}
+        
+        def depth(d, curr):
+            
+            if (d, curr) in memo:
+                return memo[(d, curr)]
+            
+            if d == n:
                 return 1
             
-            ans = 0
-            for next_square in jumps[square]:
-                ans = (ans + dp(remain - 1, next_square)) % MOD
+            temp = 0
             
-            return ans
+            
+            for i in paths[curr]:
+                temp += depth(d + 1, i)
+            
+            memo[(d, curr)] = temp
+            
+            return memo[(d, curr)] 
         
-        jumps = [
-            [4, 6],
-            [6, 8],
-            [7, 9],
-            [4, 8],
-            [3, 9, 0],
-            [],
-            [1, 7, 0],
-            [2, 6],
-            [1, 3],
-            [2, 4]
-        ]
+        temp = 0
+        for i in range(10):
+            temp += depth(1, i)
+        return temp % (10**9 + 7)
+            
+        
+#         dialer = [[True]*3]*4
+#         print(dialer)
+#         dialer[3][0] = False
+#         dialer[3][2] = False
+        
+# #         print("dialer: ", dialer)
 
-        ans = 0
-        MOD = 10 ** 9 + 7
-        for square in range(10):
-            ans = (ans + dp(n - 1, square)) % MOD
+#         dialer = []
+#         for row in range(4):
+#             temp = []
+#             for col in range(3):
+#                 temp.append(True)
+#             dialer.append(temp)
         
-        return ans
+#         dialer[3][0] = False
+#         dialer[3][2] = False
+    
+        
+#         def dp(curr, i, j):
+
+#             if i < 0 or i > 3:
+#                 return
+            
+#             if j < 0 or j > 2:
+#                 return
+            
+#             if not dialer[i][j]:
+#                 return
+            
+#             if curr == n:
+                
+#                 self.ans += 1
+#                 return
+            
+#             dp(curr + 1, i + 2, j - 1)
+#             dp(curr + 1, i - 2, j - 1)
+#             dp(curr + 1, i - 1, j + 2)
+#             dp(curr + 1, i - 1, j - 2)
+        
+#         print(dialer)
+#         for row in range(4):
+#             for col in range(3):
+#                 print(dialer[row][col], row, col)
+#                 if dialer[row][col]:
+#                     print(row, col)
+#                     dp(1, row, col)
+#         return self.ans
+
+
+    
