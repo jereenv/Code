d2cd4e8e63d31bc3893dbcaa2f01a4f437405da9
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-03 18:32:24 -0700

    Time: 38 ms (79.71%), Space: 16.3 MB (59.27%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
@@ -4,11 +4,11 @@ class Solution:
         ans = -1
         while idx < len(num) - 2:
             if num[idx] == num[idx + 1] == num[idx+2]:
-                ans = max(ans, int(num[idx] + num[idx + 1] + num[idx + 2]))
+                ans = max(ans, int(num[idx]))
                 idx += 2
             idx += 1
         if ans == -1:
             return ""
         if ans == 0:
             return "000"
-        return str(ans)
+        return str(ans)*3
