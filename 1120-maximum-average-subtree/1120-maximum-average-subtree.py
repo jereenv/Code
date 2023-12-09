d7d36b945d97a5c9e4048f7fc948b2ec79b8bea6
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-09 00:36:37 -0700

    Time: 52 ms (65.72%), Space: 19.1 MB (64.30%) - LeetHub

--- 1120-maximum-average-subtree/1120-maximum-average-subtree.py
+++ 1120-maximum-average-subtree/1120-maximum-average-subtree.py
@@ -1,26 +1,29 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maximumAverageSubtree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: float
-        """
-        self.res = 0
-
+class Solution:
+    def maximumAverageSubtree(self, root: Optional[TreeNode]) -> float:
+        
+        ans = 0
         def dfs(node):
             if not node:
-                return 0, 0
-            val_left, count_left = dfs(node.left)
-            val_right, count_right = dfs(node.right)
-            val_total = float(val_left + val_right + node.val)
-            count_total = count_left + count_right + 1
-            self.res = max(self.res, val_total / count_total)
-            return val_total, count_total
+                return 0,0
             
+            s_left, n_left = dfs(node.left)
+            s_right, n_right = dfs(node.right)
+            
+            t_nodes = n_left + n_right + 1
+            t_sum = s_left + s_right + node.val
+            t_avg = t_sum/ t_nodes
+            
+            nonlocal ans
+            ans = max(ans, t_avg)
+            
+            return t_sum, t_nodes
+        
+        
         dfs(root)
-        return self.res
+        return ans
