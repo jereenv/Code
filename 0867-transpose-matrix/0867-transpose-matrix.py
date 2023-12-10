660e9851242ced055d788850ce536351088c7e2d
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-10 00:12:45 -0700

    Time: 72 ms (68.00%), Space: 17.2 MB (22.76%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        R, C = len(matrix), len(matrix[0])
+        ans = [[None] * R for _ in range(C)]
+        for r, row in enumerate(matrix):
+            for c, val in enumerate(row):
+                ans[c][r] = val
+        return ans
+        
