28d2b5058d9468ac73e952a97f7437ea0ecd5ccf
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-14 18:02:39 -0700

    Time: 730 ms (31.89%), Space: 22.7 MB (48.41%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,9 +1,6 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
-        distances = []
-        
-        
         for idx, val in enumerate(points):
             x,y = val
             points[idx] =[(x)**2 + (y)**2,[x,y]]
