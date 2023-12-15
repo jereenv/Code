aed6b441342fb93a55c563795f84822b6901ef9e
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-14 18:00:21 -0700

    Time: 693 ms (70.06%), Space: 23.2 MB (33.15%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -2,11 +2,10 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
         distances = []
-        def getEuclidean(x1,y1):
-            return (x1)**2 + (y1)**2
+        
         
         for x,y in points:
-            distances.append([getEuclidean(x,y),[x,y]])
+            distances.append([(x)**2 + (y)**2,[x,y]])
         
         distances.sort()
         ans = []
