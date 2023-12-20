269065ec352920a14859bb0aa00f057408701b1a
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-19 20:47:26 -0700

    Time: 76 ms (9.00%), Space: 16.3 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,13 +1,15 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        small = inf
-        s_small = inf
-        for i in prices:
-            if i < small:
-                s_small = small
-                small = i
-            else:
-                s_small = min(i, s_small)
-        ans = money - small - s_small
-        return ans if ans >= 0 else money
-        
+        # Sort the Array in Increasing Order
+        prices.sort()
+
+        # Minimum Cost
+        min_cost = prices[0] + prices[1]
+
+        # We can buy chocolates only if we have enough money
+        if min_cost <= money:
+            # Return the Amount of Money Left
+            return money - min_cost
+        else:
+            # We cannot buy chocolates. Return the initial amount of money
+            return money
