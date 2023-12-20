4850274077f378710bb3e2377788a2a326bc9713
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-19 20:47:11 -0700

    Time: 75 ms (11.53%), Space: 16.4 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,6 +1,13 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        prices.sort()
-        ans = money - prices[0] - prices[1]
+        small = inf
+        s_small = inf
+        for i in prices:
+            if i < small:
+                s_small = small
+                small = i
+            else:
+                s_small = min(i, s_small)
+        ans = money - small - s_small
         return ans if ans >= 0 else money
         
