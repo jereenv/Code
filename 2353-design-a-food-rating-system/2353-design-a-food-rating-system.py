3036ccafce1dad2d150de7bc88a8f31a6cb8b67e
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2023-12-22 06:10:38 -0700

    Time: 842 ms (52.35%), Space: 50.3 MB (24.64%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -1,17 +1,4 @@
-class Food:
-    def __init__(self, food_rating, food_name):
-        # Store the food's rating.
-        self.food_rating = food_rating
-        # Store the food's name.
-        self.food_name = food_name
-
-    def __lt__(self, other):
-        # Overload the less-than operator for comparison.
-        # If food ratings are the same, sort based on their name (lexicographically smaller name food will be on top).
-        if self.food_rating == other.food_rating:
-            return self.food_name < other.food_name
-        # Sort based on food rating (bigger rating food will be on top).
-        return self.food_rating > other.food_rating
+from sortedcontainers import SortedSet
 
 class FoodRatings:
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
@@ -19,33 +6,32 @@ class FoodRatings:
         self.food_rating_map = {}
         # Map food with the cuisine it belongs to.
         self.food_cuisine_map = {}
-        # Store all food of cuisine in a priority queue (to sort them on ratings/name).
-        # Priority queue element -> Food: (food_rating, food_name)
-        self.cuisine_food_map = defaultdict(list)
+
+        # Store all food of cuisine in a set (to sort them on ratings/name)
+        # Set element -> Tuple: (-1 * food_rating, food_name)
+        self.cuisine_food_map = defaultdict(SortedSet)
 
         for i in range(len(foods)):
             # Store 'rating' and 'cuisine' of the current 'food' in 'food_rating_map' and 'food_cuisine_map' maps.
             self.food_rating_map[foods[i]] = ratings[i]
             self.food_cuisine_map[foods[i]] = cuisines[i]
-            # Insert the '(rating, name)' element into the current cuisine's priority queue.
-            heapq.heappush(self.cuisine_food_map[cuisines[i]], Food(ratings[i], foods[i]))
+            # Insert the '(-1 * rating, name)' element in the current cuisine's set.
+            self.cuisine_food_map[cuisines[i]].add((-ratings[i], foods[i]))
 
     def changeRating(self, food: str, newRating: int) -> None:
+        # Fetch cuisine name for food.
+        cuisine_name = self.food_cuisine_map[food]
+
+        # Find and delete the element from the respective cuisine's set.
+        old_element = (-self.food_rating_map[food], food)
+        self.cuisine_food_map[cuisine_name].remove(old_element)
+
         # Update food's rating in 'food_rating' map.
         self.food_rating_map[food] = newRating
-        # Insert the '(new rating, name)' element in the respective cuisine's priority queue.
-        cuisineName = self.food_cuisine_map[food]
-        heapq.heappush(self.cuisine_food_map[cuisineName], Food(newRating, food))
+        # Insert the '(-1 * new rating, name)' element in the respective cuisine's set.
+        self.cuisine_food_map[cuisine_name].add((-newRating, food))
 
     def highestRated(self, cuisine: str) -> str:
-        # Get the highest rated 'food' of 'cuisine'.
         highest_rated = self.cuisine_food_map[cuisine][0]
-
-        # If the latest rating of 'food' doesn't match with the 'rating' on which it was sorted in the priority queue,
-        # then we discard this element from the priority queue.
-        while self.food_rating_map[highest_rated.food_name] != highest_rated.food_rating:
-            heapq.heappop(self.cuisine_food_map[cuisine])
-            highest_rated = self.cuisine_food_map[cuisine][0]
-
-        # Return the name of the highest-rated 'food' of 'cuisine'.
-        return highest_rated.food_name
+        # Return name of the highest-rated 'food' of 'cuisine'.
+        return highest_rated[1]
