ce67a3a42f420e814a808a8315cde05751d00954
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-03 04:42:20 -0700

    Time: 96 ms (67.72%), Space: 19.2 MB (20.41%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -2,16 +2,17 @@ class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         idx = 0
         while idx < len(bank):
-            if int(bank[idx]) == 0:
-                bank.pop(idx)
-            else:
-                bank[idx] = bank[idx].count("1")
-                idx += 1
+            bank[idx] = bank[idx].count("1")
+            idx += 1
+        
         
         idx = 0
         ans = 0
+        prev = 0
         while idx < len(bank) - 1:
-            ans += bank[idx] * bank[idx + 1]
+            if bank[idx]:
+                prev = bank[idx]
+            ans += prev * bank[idx + 1]
             idx += 1
         return ans
             
