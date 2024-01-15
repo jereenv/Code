1351a63545bccccc4f27ae27ffb268162df47bfa
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-15 03:10:38 -0700

    Time: 44 ms (39.83%), Space: 17.4 MB (31.14%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,7 +1,6 @@
 class Solution:
     
     def compare(self, s1, s2):
-        
         idx = 0
         while idx < len(s1) and idx < len(s2):
             if s1[idx] != s2[idx]:
@@ -11,11 +10,6 @@ class Solution:
         
     def longestCommonPrefix(self, strs: List[str]) -> str:
         prev = strs[0]
-        
         for i in range(1, len(strs)):
             prev = self.compare(prev, strs[i])
-            
-        
-        
-        
         return prev
