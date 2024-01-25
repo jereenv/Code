3c5f97d976cb9ad332eb947c4d1134a9655efc00
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-24 22:01:30 -0700

    Time: 1271 ms (15.27%), Space: 308.9 MB (13.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,20 +2,20 @@ from functools import lru_cache
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
-        @lru_cache(maxsize=None)
-        def memo_solve(p1, p2):
-            
-            # Base case: If either string is now empty, we can't match
-            # up anymore characters.
-            if p1 == len(text1) or p2 == len(text2):
+        t1_MAX = len(text1)
+        t2_MAX = len(text2)
+        dp = {}
+        
+        def dfs(i, j):
+            if i == t1_MAX or j == t2_MAX:
                 return 0
+                
             
-            # Recursive case 1.
-            if text1[p1] == text2[p2]:
-                return 1 + memo_solve(p1 + 1, p2 + 1)
-            
-            # Recursive case 2.
-            else:
-                return max(memo_solve(p1, p2 + 1), memo_solve(p1 + 1, p2))
-            
-        return memo_solve(0, 0)
+            if (i, j) not in dp:
+                if text1[i] == text2[j]:
+                    dp[(i, j)] = 1 + dfs(i + 1, j + 1)
+                else:
+                    dp[(i, j)] = max(dfs(i+1, j), dfs(i, j + 1))
+            return dp[(i, j)]
+        
+        return dfs(0, 0)
