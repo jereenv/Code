7bfaadf43d64c765164e33e4235bed652f6bf88e
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-26 13:26:07 -0700

    Time: 88 ms (70.52%), Space: 22.1 MB (45.66%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -1,28 +1,25 @@
 class Solution:
-    def findPaths(self, m: int, n: int, N: int, x: int, y: int) -> int:
-        M = 1000000000 + 7
-        dp = [[0] * n for _ in range(m)]
-        dp[x][y] = 1
-        count = 0
-
-        for moves in range(1, N + 1):
-            temp = [[0] * n for _ in range(m)]
-
-            for i in range(m):
-                for j in range(n):
-                    if i == m - 1:
-                        count = (count + dp[i][j]) % M
-                    if j == n - 1:
-                        count = (count + dp[i][j]) % M
-                    if i == 0:
-                        count = (count + dp[i][j]) % M
-                    if j == 0:
-                        count = (count + dp[i][j]) % M
-                    temp[i][j] = (
-                        ((dp[i - 1][j] if i > 0 else 0) + (dp[i + 1][j] if i < m - 1 else 0)) % M +
-                        ((dp[i][j - 1] if j > 0 else 0) + (dp[i][j + 1] if j < n - 1 else 0)) % M
-                    ) % M
-
-            dp = temp
-
-        return count
+    def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
+        
+        ans = [0]
+        
+        vis = {}
+        def dfs(i, j, p):
+            if (i == m or i < 0 or j == n or j < 0):
+                
+                return 1
+            
+            if p == maxMove:
+                return 0
+            
+            
+            p += 1
+            
+            if (i, j, p) not in vis:
+                vis[(i, j, p)] = dfs(i + 1, j, p) + dfs(i, j + 1, p) + dfs(i - 1, j, p) + dfs(i, j - 1, p)
+            
+            return vis[(i, j, p)]
+            
+            
+       
+        return dfs(startRow, startColumn, 0) % (10**9 + 7)
