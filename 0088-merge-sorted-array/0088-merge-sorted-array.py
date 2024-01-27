12feb5dd952a60a939166075dd37e9b30fcb6d2f
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-27 14:28:53 -0700

    Time: 42 ms (60.25%), Space: 16.6 MB (56.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,9 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # Write the elements of num2 into the end of nums1.
-        for i in range(n):
-            nums1[i + m] = nums2[i]
-        
-        # Sort nums1 list in-place.
-        nums1.sort()
+        idx = m + n - 1
+        n -= 1
+        m -= 1
+        while True:
+            if n < 0:
+                break
+            
+            if m >= 0 and nums1[m] > nums2[n]:
+                nums1[idx] = nums1[m]
+                m -= 1
+            else:
+                nums1[idx] = nums2[n]
+                n -= 1
+            idx -= 1
+                
+        else:
+            while n >= 0:
+                nums1[idx] = nums2[n]
+                idx -=1 
+                n -=1
