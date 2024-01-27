519694ed1f6c705a0b83c9cb26f0913312d0baa1
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-27 14:14:39 -0700

    Time: 42 ms (60.25%), Space: 16.6 MB (56.62%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,20 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # Write the elements of num2 into the end of nums1.
+        for i in range(n):
+            nums1[i + m] = nums2[i]
         
-        # Set p1 and p2 to point to the end of their respective arrays.
-        p1 = m - 1
-        p2 = n - 1
-    
-        # And move p backwards through the array, each time writing
-        # the smallest value pointed at by p1 or p2.
-        for p in range(n + m - 1, -1, -1):
-            if p2 < 0:
-                break
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
-                
+        # Sort nums1 list in-place.
+        nums1.sort()
