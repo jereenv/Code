6d6c5c4fba44f16bbc3eaa1eb2d12bff968e3fab
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-28 14:10:26 -0700

    Time: 45 ms (94.78%), Space: 16.5 MB (66.00%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,9 +4,8 @@ class Solution:
         k = 1
         dic[nums[0]] += 1
         for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1] or dic[nums[i]] < 2:
+            if dic[nums[i]] < 2:
                 nums[k] = nums[i]
                 k += 1
             dic[nums[i]] += 1
-        print(dic)
         return k
