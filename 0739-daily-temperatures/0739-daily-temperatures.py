a46a9e57b1b3a81f17668af97bde352bd17b5aed
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-01-30 17:11:34 -0700

    Time: 851 ms (98.87%), Space: 30.1 MB (97.14%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,7 +1,7 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
-        ans = [0 for _ in range(len(temperatures))]
+        ans = [0] * len(temperatures)
         
         stack = []
         
