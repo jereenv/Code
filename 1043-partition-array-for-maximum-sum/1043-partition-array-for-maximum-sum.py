2b89345ea18609cb22f7e2376ffecfb8b85f96b0
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-02-02 22:29:51 -0700

    Time: 1632 ms (11.56%), Space: 17.6 MB (35.45%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr, k):
-        N = len(arr)
-        K = k + 1
-
-        dp = [0] * K
-
-        for start in range(N - 1, -1, -1):
-            curr_max = 0
-            end = min(N, start + k)
-
-            for i in range(start, end):
-                curr_max = max(curr_max, arr[i])
-                dp[start % K] = max(dp[start % K], dp[(i + 1) % K] + curr_max * (i - start + 1))
-
-        return dp[0]
+        n = len(arr)
+        memo = {}
+        
+        def dp(idx):
+            if idx >= n:
+                return 0
+            
+            if idx not in memo:
+                temp = [max(arr[idx : idx + i]) * i + dp(idx + i) for i in range(1, min(k, n - idx) + 1)]
+                print(temp)
+                memo[idx] = max(temp)
+            return memo[idx]
+        
+        return dp(0)
