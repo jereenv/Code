2130ed69cf188571af2edb7deee93146c8a91fc1
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-04 13:07:12 -0700

    Time: 40 ms (61.73%), Space: 16.5 MB (94.12%) - LeetHub

---
+++ 3062-winner-of-the-linked-list-game/3062-winner-of-the-linked-list-game.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def gameResult(self, head: Optional[ListNode]) -> str:
+        ans = 0
+        
+        while head:
+            if head.val > head.next.val:
+                ans += 1
+            else:
+                ans -= 1
+            head = head.next.next
+        return "Even" if ans > 0 else "Tie" if ans == 0 else "Odd"
+        
