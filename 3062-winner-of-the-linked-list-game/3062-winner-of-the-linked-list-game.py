9b51c91040fb911df335ad1b1ee15cad34ef88c1
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-04 13:10:13 -0700

    Time: 32 ms (94.53%), Space: 16.5 MB (94.12%) - LeetHub

--- 3062-winner-of-the-linked-list-game/3062-winner-of-the-linked-list-game.py
+++ 3062-winner-of-the-linked-list-game/3062-winner-of-the-linked-list-game.py
@@ -5,10 +5,14 @@
 #         self.next = next
 class Solution:
     def gameResult(self, head: Optional[ListNode]) -> str:
-        ans = 0
+        ans, node = 0, head
         
-        while head:
-            ans += (head.val > head.next.val) - (head.val < head.next.val)
-            head = head.next.next
+        while node:
+            if node.val > node.next.val:
+                ans += 1
+            else:
+                ans -= 1
+            node = node.next.next
+            
         return "Even" if ans > 0 else "Tie" if ans == 0 else "Odd"
         
