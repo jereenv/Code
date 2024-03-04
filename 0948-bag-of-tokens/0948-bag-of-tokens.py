14314d05f5ce6710fd322ae6c9f07cafa74b9d93
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-03 23:13:31 -0700

    Time: 54 ms (53.66%), Space: 16.7 MB (94.31%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -1,28 +1,21 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
-        low = 0
-        high = len(tokens) - 1
-        score = 0
+        
+        ans = score = 0
         tokens.sort()
-
-        while low <= high:
-            # When we have enough power, play lowest token face-up
-            if power >= tokens[low]:
+        l, r = 0, len(tokens) - 1
+        
+        while l <= r:
+            if power >= tokens[l]:
+                power -= tokens[l]
+                l += 1
                 score += 1
-                power -= tokens[low]
-                low += 1
-
-            # We don't have enough power to play a token face-up
-            # If there is at least one token remaining,
-            # and we have enough score, play highest token face-down
-            elif low < high and score > 0:
+                ans = max(ans, score)
+            elif score > 0:
+                power += tokens[r]
                 score -= 1
-                power += tokens[high]
-                high -= 1
-
-            # We don't have enough score, power, or tokens 
-            # to play face-up or down and increase our score
+                r -= 1
             else:
-                return score
+                break
 
-        return score       
+        return ans       
