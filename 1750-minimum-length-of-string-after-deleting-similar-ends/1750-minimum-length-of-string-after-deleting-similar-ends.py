e683625a48cb09226ba2ccad83041bdf470d6bdd
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-04 21:41:07 -0700

    Time: 62 ms (85.78%), Space: 17.4 MB (75.83%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -1,19 +1,20 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        begin = 0
-        end = len(s) - 1
-
-        # Delete similar ends until the ends differ or they meet in the middle
-        while begin < end and s[begin] == s[end]:
-            c = s[begin]
-
-            # Delete consecutive occurrences of c from prefix
-            while begin <= end and s[begin] == c:
-                begin += 1
-
-            # Delete consecutive occurrences of c from suffix
-            while end > begin and s[end] == c:
-                end -= 1
-
-        # Return the number of remaining characters
-        return end - begin + 1
+        ans = len(s)
+        
+        l, r = 0, ans - 1
+        
+        while l < r:
+            curr = s[l]
+            
+            if s[r] != curr:
+                break
+            
+            while s[r] == curr and l < r:
+                r -= 1
+            
+            while s[l] == curr and l <= r:
+                l += 1
+        return r - l + 1
+                
+        
