5198361eead2cab01c167f0bca5e9c2cb5c91f74
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-12 21:05:41 -0700

    Time: 41 ms (76.39%), Space: 16.5 MB (92.38%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,14 +1,6 @@
 class Solution:
-    def pivotInteger(self, n: int) -> int:
-        l, r = 1, n
-        s1, s2 = 0, 0
-        while l != r:
-            if s1 < s2:
-                s1 += l
-                l += 1
-            else:
-                s2 += r
-                r -= 1
-        return l if s1 == s2 else -1
-        
-        
+  def pivotInteger(self, n: int) -> int:
+        sum = (n * (n + 1) // 2)
+        pivot = int(math.sqrt(sum))
+        # If pivot * pivot is equal to sum (pivot found) return pivot, else return -1
+        return pivot if pivot * pivot == sum else -1
