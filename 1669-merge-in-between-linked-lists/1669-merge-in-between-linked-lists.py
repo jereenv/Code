6815d79c4c2f269bfa5b3efd0453e8566f8546ae
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-20 02:25:58 -0700

    Time: 192 ms (58.89%), Space: 21.1 MB (70.92%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.py
@@ -1,35 +1,22 @@
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
-        merge_array = []
+        start = ListNode()
+        end = list1
+
+        # Set start to node a - 1 and end to node b
+        for index in range (b):
+            if index == a - 1:
+                start = end
+            end = end.next
+
+        # Connect the start node to list2
+        start.next = list2
+
+        # Find the tail of list2
+        while (list2.next is not None):
+            list2 = list2.next
+        # Set the tail of list2 to end.next
+        list2.next = end.next
+        end.next = None
         
-        # Add list1 node values before `a` to the array.
-        index = 0
-        current1 = list1
-        while index < a:
-            merge_array.append(current1.val)
-            current1 = current1.next
-            index += 1
-
-        # Add list2 node values to the array
-        current2 = list2
-        while current2 is not None:
-            merge_array.append(current2.val)
-            current2 = current2.next
-
-        # Find node b + 1
-        while index < b + 1:
-            current1 = current1.next
-            index += 1
-
-        # Add list1 node values after `b` to the array.
-        while current1 is not None:
-            merge_array.append(current1.val)
-            current1 = current1.next
-
-        # Build a linked list with the result by iterating over the array
-        # in reverse order and inserting new nodes to the front of the list
-        result_list = None
-        for i in range(len(merge_array)):
-            new_node = ListNode(merge_array.pop(), result_list)
-            result_list = new_node
-        return result_list
+        return list1
