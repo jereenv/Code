fa5c653fe7d9eb015bd3aa9d2dce0150a5b7bbf8
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-03-25 19:14:04 -0700

    Time: 302 ms (42.47%), Space: 30.4 MB (55.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,16 +1,47 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         
-        n = len(nums)
-        seen = [False] * (n)
+#         n = len(nums)
         
-        for num in nums:
-            if n >= num > 0:
-                seen[num - 1] = True
+#         for i in range(n):
+#             if nums[i] < 0:
+#                 nums[i] = 0
         
-        for idx, val in enumerate(seen):
-            if not val:
-                return idx + 1
         
-        return n + 1
+#         for i in range(n):
+#             num = abs(nums[i])
+#             if 0 < num < n + 1:
+                
+#                 if nums[num - 1] > 0:
+#                     nums[num - 1] *= -1
+#                 elif nums[num - 1] == 0:
+#                     nums[num - 1] = -1 * (n + 1)
+                
         
+#         for i in range(1, n + 1):
+#             if nums[i - 1] > 0:
+#                 return i
+        
+#         return n + 1
+
+        
+        for i in range(len(nums)):
+            if nums[i] < 0:
+                nums[i] = 0
+            
+        
+        for i in range(len(nums)):
+            val = abs(nums[i])
+            if 1 <= val <= len(nums):
+                if nums[val - 1] > 0:
+                    nums[val - 1] *= -1
+                
+                elif nums[val - 1] == 0:
+                    nums[val - 1] = -1 * (len(nums) + 1)
+            
+        
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] >= 0:
+                return i
+        
+        return len(nums) + 1
