e1b1e709a1b8271c15e72797e2361c793c85dd93
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-04-05 03:01:23 -0700

    Time: 42 ms (27.78%), Space: 16.7 MB (11.30%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,21 +1,15 @@
 class Solution:
-    def makeGood(self, s: str) -> str:
-        # if s has less than 2 characters, we just return itself.
-        while len(s) > 1:
-            # 'find' records if we find any pair to remove.
-            find = False
-            
-            # Check every two adjacent characters, curr_char and next_char.
-            for i in range(len(s) - 1):
-                curr_char, next_char = s[i], s[i + 1]
-                
-                # If they make a pair, remove them from 's' and let 'find = True'.
-                if abs(ord(curr_char) - ord(next_char)) == 32:
-                    s = s[:i] + s[i + 2:]
-                    find = True
-                    break
-            
-            # If we cannot find any pair to remove, break the loop. 
-            if not find:
-                break
-        return s
+    def makeGood(self, w: str) -> str:
+        s = deque()
+        s.append(w[0])
+        n = len(w)
+        i = 1
+        while i < len(w):
+            if s and s[-1].upper() == w[i].upper() and s[-1] != w[i]:
+                s.pop()
+            else:
+                s.append(w[i])
+            i += 1
+        
+        return "".join(s)
+        
