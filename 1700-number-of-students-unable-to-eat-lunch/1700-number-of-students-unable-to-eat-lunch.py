6c821177a80a73d19fd665f1c1f5a5a92e1fca61
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-04-08 02:12:33 -0700

    Time: 46 ms (19.81%), Space: 16.6 MB (16.84%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,21 +1,34 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         
-        st = deque(students)
-        sd = deque(reversed(sandwiches))
+        dic = Counter(students)
         
-        t = len(st)
-        
-        n = t
-        
-        while n > 0:
-            if st[-1] == sd[-1]:
-                st.pop()
-                sd.pop()
-                n = len(st)
+        for idx, val in enumerate(sandwiches):
+            if dic[val] > 0:
+                dic[val] -= 1
             else:
-                st.appendleft(st.pop())
-                n -= 1
-        return len(st)
+                return len(sandwiches) - idx
+        return 0
+            
+        
+        
+        
+        
+#         st = deque(students)
+#         sd = deque(reversed(sandwiches))
+        
+#         t = len(st)
+        
+#         n = t
+        
+#         while n > 0:
+#             if st[-1] == sd[-1]:
+#                 st.pop()
+#                 sd.pop()
+#                 n = len(st)
+#             else:
+#                 st.appendleft(st.pop())
+#                 n -= 1
+#         return len(st)
                 
         
