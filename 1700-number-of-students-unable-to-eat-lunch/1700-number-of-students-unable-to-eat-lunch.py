1dad756cbd2b3ae96520967499e3b27f5b8ec865
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-04-08 02:10:38 -0700

    Time: 31 ms (95.68%), Space: 16.6 MB (16.84%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,31 +1,22 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        circle_student_count = 0
-        square_student_count = 0
-
-        # Count the number of students who want each type of sandwich
-        for student in students:
-            if student == 0:
-                circle_student_count += 1
+        
+        st = deque(students)
+        sd = deque(reversed(sandwiches))
+        
+        t = len(st)
+        
+        n = t
+        
+        while n > 0:
+            if st[-1] == sd[-1]:
+                st.pop()
+                sd.pop()
+                n = len(st)
             else:
-                square_student_count += 1
-
-        # Serve sandwiches to students
-        for sandwich in sandwiches:
-
-            # No student wants the circle sandwich on top of the stack
-            if sandwich == 0 and circle_student_count == 0:
-                return square_student_count
-
-            # No student wants the square sandwich on top of the stack
-            if sandwich == 1 and square_student_count == 0:
-                return circle_student_count
-
-            # Decrement the count of the served sandwich type
-            if sandwich == 0:
-                circle_student_count -= 1
-            else:
-                square_student_count -= 1
-
-        # Every student received a sandwich
-        return 0
+                st.appendleft(st.pop())
+                n -= 1
+        print(st, sd)
+        return len(st)
+                
+        
