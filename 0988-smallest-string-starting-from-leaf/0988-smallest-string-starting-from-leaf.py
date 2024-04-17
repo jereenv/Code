55b39b97bc04f93ad88ce02d0de3da18ca48926e
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-04-17 00:30:02 -0700

    Time: 41 ms (71.88%), Space: 17.6 MB (88.96%) - LeetHub

--- 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
+++ 0988-smallest-string-starting-from-leaf/0988-smallest-string-starting-from-leaf.py
@@ -1,30 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
-        self.smallest_string = ""
-        self.dfs(root, "")
-        return self.smallest_string
-
-    # Helper function to find the lexicographically smallest string
-    def dfs(self, root, current_string):
-        # If the current node is NULL, return
-        if not root:
-            return
-
-        # Construct the current string by appending 
-        # the character corresponding to the node's value
-        current_string = chr(root.val + ord('a')) + current_string
-
-        # If the current node is a leaf node
-        if not root.left and not root.right:
-            # If the current string is smaller than the result 
-            # or if the result is empty
-            if not self.smallest_string or self.smallest_string > current_string:
-                self.smallest_string = current_string
-
-        # Recursively traverse the left subtree
-        if root.left:
-            self.dfs(root.left, current_string)
         
-        # Recursively traverse the right subtree
-        if root.right:
-            self.dfs(root.right, current_string)
+        res = ["z"*85000]
+        
+        # def dfs(curr)
+        
+        #a = 97
+        #
+        
+        def dfs(curr, node):
+            if not node:
+                res[0] = min(curr, res[0])
+                return
+            
+            if not node.left and node.right:
+                dfs(chr(node.val + 97) + curr, node.right)
+            elif not node.right and node.left:
+                dfs(chr(node.val + 97) + curr, node.left)
+            else:
+                dfs(chr(node.val + 97) + curr, node.right)
+                dfs(chr(node.val + 97) + curr, node.left)
+                
+            
+
+        dfs("", root)
+        return res[0]
