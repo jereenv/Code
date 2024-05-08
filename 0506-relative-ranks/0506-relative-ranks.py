56b2bb3f6fd5c266c32bc6b0d222bbe9679ebcf5
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-05-08 02:03:54 -0700

    Time: 503 ms (7.84%), Space: 32 MB (5.59%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        
+        inverse = [-1 for i in range(10**6)]
+        
+        for idx, val in enumerate(score):
+            inverse[val] = idx
+        
+        ans = [-1 for i in range(len(score))]
+        
+        medals = ["Bronze Medal","Silver Medal","Gold Medal"]
+
+        curr = 1
+        for idx in reversed(inverse):
+            if idx != -1:
+                if medals:
+                    ans[idx] = medals.pop()
+                else:
+                    ans[idx] = str(curr)
+                curr += 1
+        
+        return ans
+                    
+        
+        
+        
