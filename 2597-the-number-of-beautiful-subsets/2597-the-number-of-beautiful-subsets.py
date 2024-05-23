01e6a21a9032b3e52634c892854d8dd7a2e7842a
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-05-22 23:15:52 -0700

    Time: 8181 ms (5.07%), Space: 16.5 MB (96.46%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -0,0 +1,30 @@
+class Solution:
+    
+    ans = -1
+    
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        
+        
+        dic = defaultdict(int)
+        n = len(nums)
+        
+        def backtrack(idx, curr):
+            if idx == n:
+                self.ans += 1
+                return
+            
+            backtrack(idx + 1, curr.copy())
+            
+            if curr[nums[idx] - k] == 0 and curr[nums[idx] + k] == 0:
+                curr[nums[idx]] += 1
+                backtrack(idx + 1, curr.copy())
+            
+            
+        backtrack(0, dic)
+        return self.ans
+    
+    
+    
+    def isBeautiful(self, arr, k):
+        pass
+        
