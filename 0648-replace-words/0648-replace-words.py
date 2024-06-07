63afa28895eaa31e7798f715e1af00bb19e7ebc3
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-06-07 00:54:26 -0700

    Time: 348 ms (15.15%), Space: 24.8 MB (98.64%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,32 +1,19 @@
 class Solution:
-    def replaceWords(self, dictionary: List[str], s1: str) -> str:
-        
-        s = set(dictionary)
-        
-        n = len(s1)
-        
-        ans = []
-        l = 0
-        while l < n:
-            
-            idx = l
-            
-            while idx < n and s1[idx] != " ":
-                
-                
-                if s1[l:idx + 1] in s:
-                    ans.append(s1[l:idx + 1])
-                    
-                    while idx < n:
-                        if s1[idx] == " ":
-                            break
-                        idx += 1
-                    break
-                idx += 1
-            else:
-                ans.append(s1[l: idx])
-            
-            l = idx + 1
-        return " ".join(ans)
-            
-        
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        word_array = sentence.split()
+        dict_set = set(dictionary)
+
+        def shortest_root(word, dict_set):
+            # Find the shortest root of the word in the dictionary
+            for i in range(len(word)):
+                root = word[0:i]
+                if root in dict_set:
+                    return root
+            # There is not a corresponding root in the dictionary
+            return word
+
+        # Replace each word in sentence with the corresponding shortest root
+        for word in range(len(word_array)):
+            word_array[word] = shortest_root(word_array[word], dict_set)
+
+        return " ".join(word_array)
