ed10581b2a45bde0bac42fc8851c9192a2f37356
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-06-06 23:03:57 -0700

    Time: 403 ms (10.53%), Space: 24.9 MB (81.26%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,33 @@
+class Solution:
+    def replaceWords(self, dictionary: List[str], s1: str) -> str:
+        
+        s = set(dictionary)
+        
+        n = len(s1)
+        
+        ans = []
+        l = 0
+        while l < n:
+            
+            idx = l
+            
+            while idx < n and s1[idx] != " ":
+                
+                
+                if s1[l:idx + 1] in s:
+                    ans.append(s1[l:idx + 1])
+                    
+                    while idx < n:
+                        if s1[idx] == " ":
+                            break
+                        idx += 1
+                    break
+                idx += 1
+            else:
+                ans.append(s1[l: idx])
+            
+            l = idx + 1
+        print(ans)
+        return " ".join(ans)
+            
+        
