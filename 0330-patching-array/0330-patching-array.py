78693f849160483ca4fd7397902e54d181b83821
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-06-16 15:35:43 -0700

    Time: 57 ms (55.71%), Space: 16.7 MB (69.55%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        
+        i, output, upto = 0, 0, 0
+        N = len(nums)
+        while upto < n:
+            if i < len(nums) and nums[i] <= upto + 1:
+                upto += nums[i]
+                i += 1
+            else:
+                output += 1
+                upto += upto + 1
+        
+        return output
