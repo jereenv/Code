b10ea283383b66899e045920294bf3931b7964d5
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-06-17 02:10:29 -0700

    Time: 85 ms (76.46%), Space: 16.5 MB (39.72%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -3,14 +3,27 @@ class Solution:
         
         cs = int(c ** 0.5)
         
-        s = set()
-        for i in range(cs + 1):
-            s.add(i**2)
+#         s = set()
+#         for i in range(cs + 1):
+#             s.add(i**2)
         
-        for i in range(cs + 1):
-            if (c - i**2) in s:
+#         for i in range(cs + 1):
+#             if (c - i**2) in s:
+#                 return True
+        
+#         return False
+
+        
+        l, r = 0, cs
+        
+        while l <= r:
+            t = l**2 + r**2
+            if t == c:
                 return True
-        
+            elif t > c:
+                r -= 1
+            else:
+                l += 1
         return False
                 
         
