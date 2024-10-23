87034176f6d160b9189e71f418fd34db0b7b13d7
Jereen Valsson <58659184+jereenv@users.noreply.github.com>
2024-10-23 04:01:31 -0400

    Time: 369 ms (92.31%), Space: 57.5 MB (80.07%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def replaceValueInTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        q = [[root, 0]]
+        
+        
+        while q:
+            level = []
+            
+            for node, _ in q:
+                if node.left:
+                    level.append([node.left, node.right.val if node.right else 0] )
+                if node.right:
+                    level.append([node.right, node.left.val if node.left else 0])
+        
+            s = sum([x.val for x, _ in level])
+            
+            print([[x.val, _] for x, _ in level], s)
+            
+            for node, cuz in level:
+                node.val = s - node.val - cuz
+            
+            q = level
+        root.val = 0
+        return root
+                
+        
